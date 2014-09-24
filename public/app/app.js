ea48691c3d7b97f2938cb22144d85db257c0608e
vishwa89 <vishwanath89@gmail.com>
2014-09-24 14:47:43 +0530

    5 pages

--- public/app/app.js
+++ public/app/app.js
@@ -9,6 +9,10 @@ uvic
 
 	$routeProvider
 	.when('/', { templateUrl: '/pages/index' })
+	.when('/immigration', { templateUrl: '/pages/immigration-home' })
+	.when('/immigration/student_visa', { templateUrl: '/pages/student-visa' })
+	.when('/education', { templateUrl: '/pages/education-home' })
+	.when('/education/articles', { templateUrl: '/pages/article-index' })
 	.when('/articles', { templateUrl: '/pages/articles' })
 	.when('/article-:id.html', { templateUrl: '/pages/article' })
 	.when('/search', { templateUrl: '/pages/search' })
